f70c6156cf370088fa982ba6ac115c682eec50f8
daniel rios <misterrios@gmail.com>
2025-08-26 16:51:29 +0200

    feat: add fast_api_app

---
+++ fast_api_app/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, HTTPException
+from models import Author, AuthorPublic, Book, BookPublic, Card  # noqa: F401
+from sqlalchemy.orm import selectinload
+from sqlmodel import Session, SQLModel, create_engine, select
+
+app = FastAPI()
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+SQLModel.metadata.create_all(engine)
+
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+@app.get("/api/book/{id}", response_model=BookPublic)
+async def get_single_book(id):
+    with Session(engine) as session:
+        statement = select(Book).where(Book.id == id).options(selectinload(Book.author))
+        book = session.exec(statement).one()
+        if not book:
+            raise HTTPException(status_code=404, detail="Book not found")
+        return book
+
+
+@app.get("/api/books", response_model=list[BookPublic])
+async def get_all_books():
+    with Session(engine) as session:
+        select_statement = (
+            select(Book).options(selectinload(Book.author)).order_by(Book.title)
+        )
+        books = session.exec(select_statement).all()
+        return books
---
+++ fast_api_app/models.py
@@ -0,0 +1,47 @@
+from sqlmodel import Field, Relationship, SQLModel
+
+
+class AuthorBase(SQLModel):
+    first_name: str = Field(max_length=50)
+    last_name: str = Field(max_length=50)
+
+
+class Author(AuthorBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    books: list["Book"] = Relationship(back_populates="author")
+
+
+class AuthorPublic(AuthorBase):
+    id: int
+
+
+class BookBase(SQLModel):
+    title: str = Field(max_length=255)
+    price: float
+    summary: str | None
+
+    author_id: int | None = Field(default=None, foreign_key="author.id")
+
+
+class Book(BookBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    author: Author | None = Relationship(back_populates="books")
+
+
+class BookPublic(BookBase):
+    id: int
+    author: AuthorPublic
+
+
+class CardBase(SQLModel):
+    name: str = Field(max_length=50)
+    creator: str = Field(max_length=100)
+    price: float
+
+
+class Card(CardBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+
+
+class CardPublic(CardBase):
+    id: int
