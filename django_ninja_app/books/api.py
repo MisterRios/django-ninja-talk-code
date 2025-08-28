cc83b81a99d32a4b0396580363edae10f5e7b2f1
daniel rios <misterrios@gmail.com>
2025-08-28 17:28:08 +0200

    feat: make django-ninja app async

--- django_ninja_app/books/api.py
+++ django_ninja_app/books/api.py
@@ -1,17 +1,25 @@
 from books.models import Book
 from books.schemas import BookSchema
+from django.http import Http404
 from ninja import Router
-
-from django.shortcuts import get_object_or_404
 
 router = Router()
 
 
 @router.get("/{id}", response=BookSchema, url_name="single_book")
-def get_single_book(request, id):
-    return get_object_or_404(Book, pk=id)
+async def get_single_book(request, id):
+    try:
+        book = await Book.objects.select_related("author").aget(id=id)
+        if not book:
+            raise Http404("Book not found")
+    except Book.DoesNotExist:
+        raise Http404("Book not found")
+    else:
+        return book
 
 
 @router.get("/", response=list[BookSchema], url_name="all_books")
-def get_all_books(request):
-    return Book.objects.order_by("title")
+async def get_all_books(request):
+    return [
+        book async for book in Book.objects.select_related("author").order_by("title")
+    ]
